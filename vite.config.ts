250e7c6da387c5279e0af0d22038d44124e00aae
yokawase <yokawase@gmail.com>
2026-06-01 22:39:36 +0900

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Mt.Chokai-project/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
